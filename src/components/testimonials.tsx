d79c4b374957845944d21457a5d38ef767613abb
julianlbs <julianlimalbs@gmail.com>
2022-10-26 18:43:13 -0300

    commit

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -8,15 +8,17 @@ import { IconContext } from "react-icons";
 
 export default function Testimonials() {
 	return (
-		<section className="container">
+		<section className="container bg-[#0f62ff]">
 			<div className=" mb-12">
-				<h3 className="text-center text-primary uppercase">Testimonials</h3>
-				<p className="text-center text-secondary">
+				<h3 className="text-center text-primary-content uppercase">
+					Testimonials
+				</h3>
+				<p className="text-center text-secondary-content">
 					What our clients are saying
 				</p>
 			</div>
 
-			<div className="w-72 sm:w-[450px] md:w-[650px] mx-auto">
+			<div className="w-72 sm:w-[450px] md:w-[650px] mx-auto pb-10">
 				<Slider
 					dots={true}
 					infinite={true}
@@ -24,12 +26,12 @@ export default function Testimonials() {
 					slidesToShow={1}
 					slidesToScroll={1}
 					className="none"
-					prevArrow={<PreviousBtn />}
-					nextArrow={<NextBtn />}
+					// prevArrow={<PreviousBtn />}
+					// nextArrow={<NextBtn />}
 				>
 					<div className="!flex !flex-col !gap-5 !justify-center !items-center !pl-16 !pr-12">
 						<Avatar />
-						<p className="text-center">
+						<p className="text-center text-white">
 							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
 							ullam enim maiores illo cumque porro doloremque dolorum quisquam
 							vitae recusandae ad nesciunt, numquam animi dolore odio, eius
@@ -47,7 +49,7 @@ export default function Testimonials() {
 
 					<div className="!flex !flex-col !gap-5 !justify-center !items-center !px-12">
 						<Avatar />
-						<p className="text-center">
+						<p className="text-center text-white">
 							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
 							ullam enim maiores illo cumque porro doloremque dolorum quisquam
 							vitae recusandae ad nesciunt, numquam animi dolore odio, eius
